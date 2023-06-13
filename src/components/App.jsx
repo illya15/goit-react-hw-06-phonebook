2c8a12caa4a379f578849228253051b95b7caefc
illya15 <113913094+illya15@users.noreply.github.com>
2023-06-13 22:31:17 +0300

    hw6 8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,15 @@
-// import { useEffect } from 'react';
-// import { nanoid } from 'nanoid';
 import { Form } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-import { useDispatch,useSelector } from 'react-redux';
-import {
-  changeFilterAction,
-  deleteContactAction,
-  setContactsAction,
-} from 'redux/actions';
-
-
-
-export function App() {
-
-  // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(localStorage.getItem('contacts'));
-  // });
-
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-    const { contacts, filter } = useSelector(
-      state => (state.contacts)
-     
-    );
- console.log(contacts);
-    console.log(filter);
-    const dispatch = useDispatch();
-    // useEffect(() => {
-    //   localStorage.setItem('contacts', JSON.stringify(contacts));
-    // }, [contacts]);
-
-
-  const addContact = (name, number) => {
-    const duplicate = contacts.find(contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase());
-    if (duplicate) {
-      alert('A customer already exists ');
-      return;
-    }
-    dispatch(setContactsAction(contacts, name, number));
-    // setContacts(prev => [...prev, { id: nanoid(), name, number }]);
-  };
-
-  const changeFilter = e => {
-
-        dispatch(changeFilterAction(e.currentTarget.value));
-    // setFilter(e.currentTarget.value);
-  };
-
-  const deleteContact = id => {
-    const filterId = contacts.filter(contact => contact.id !== id);
-    dispatch(deleteContactAction(filterId));
-    // setContacts([...filterId]);
-  };
-
+export const App = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: '#010101',
-      }}
-    >
-      <h2>Phonebook</h2>
-      <Form onSubmit={addContact} />
+    <div>
+      <h1>Phonebook</h1>
+      <Form />
       <h2>Contacts</h2>
-      <Filter onChange={changeFilter} value={filter} />
-      <ContactList
-        onClick={deleteContact}
-        contacts={contacts}
-        filter={filter}
-      />
+      <Filter />
+      <ContactList />
     </div>
   );
-}
+};
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,62 @@
-import css from './ContactList.module.css'
+// import css from './ContactList.module.css'
 
-export const ContactList = ({ onClick, contacts, filter }) => {
+// export const ContactList = ({ onClick, contacts, filter }) => {
+//   const filterName = contacts.filter(contact =>
+//     contact.name.includes(filter)
+//   );
+//   return (
+//     <ul children={css.list}>
+//       {filterName.map(contact => (
+//         <li className={css.item} key={contact.id}>
+//           <p>
+//             {contact.name}: {contact.number}
+//           </p>
+//           <button className={css.button} type="button" onClick={() => onClick(contact.id)}>
+//             Delete
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+import { useDispatch, useSelector } from 'react-redux';
+// import { ListItem, Button, List } from './ContactList.styled';
+import css from './ContactList.module.css';
+import { deleteContacts } from 'redux/contactsSlice';
+export const ContactList = () => {
+  const {
+    contacts: { contacts },
+    filter,
+  } = useSelector(state => state);
+
+  const dispatch = useDispatch();
   const filterName = contacts.filter(contact =>
-    contact.name.includes(filter)
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
+
+  const deleteContact = id => {
+    dispatch(deleteContacts(id));
+  };
+
   return (
+    // <List>
     <ul children={css.list}>
       {filterName.map(contact => (
+        // <ListItem key={contact.id}>
         <li className={css.item} key={contact.id}>
           <p>
             {contact.name}: {contact.number}
           </p>
-          <button className={css.button} type="button" onClick={() => onClick(contact.id)}>
+          <button
+            className={css.button}
+            type="button"
+            onClick={() => deleteContact(contact.id)}
+          >
             Delete
           </button>
         </li>
       ))}
     </ul>
+    // {/* </List> */}
   );
 };
