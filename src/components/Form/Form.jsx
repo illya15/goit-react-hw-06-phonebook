61adc64d71883fa8c17a5a419661a34cbe502f0e
illya15 <113913094+illya15@users.noreply.github.com>
2023-06-14 22:08:42 +0300

    hw 6 16

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -55,8 +55,7 @@ export const Form = () => {
           type="text"
           name="name"
           pattern="^[A-Za-z\u0080-\uFFFF ']+$"
-          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={handleChange}
           value={name}
