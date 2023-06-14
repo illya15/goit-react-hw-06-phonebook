76c26df35c0502579bae4a564ef2312b91a5b653
illya15 <113913094+illya15@users.noreply.github.com>
2023-06-14 21:57:13 +0300

    hw6 15

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -54,27 +54,28 @@ export const Form = () => {
         <input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[A-Za-z\u0080-\uFFFF ']+$"
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={handleChange}
           value={name}
         />
       </label>
-      <br/>
+      <br />
       <label>
         Number <br />
         <input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^(\+?[0-9.\(\)\-\s]*)$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={handleChange}
           value={number}
         />
       </label>
-      <br/>
+      <br />
       <button type="submit">Add contact</button>
     </form>
   );
